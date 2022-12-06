f31b65a3efebc0f59a75608eba5e95a1f7cf649a
Giri Putra Adhittana <giri.adhittana@icloud.com>
2022-12-06 23:27:39 +0700

    test

--- users_grpc.pb.go
+++ users_grpc.pb.go
@@ -13,7 +13,7 @@ import (
 	status "google.golang.org/grpc/status"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
-
+//a
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
 // Requires gRPC-Go v1.32.0 or later.
